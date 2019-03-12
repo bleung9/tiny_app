b9a300fbec3018f1e855d5ebde0977bf31486541
Bosco <bleung9@gmail.com>
2019-03-12 23:57:06 +0000

    finished URL shortening 1

--- express_server.js
+++ express_server.js
@@ -9,15 +9,35 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/urls", (req, res) => {
-  let templateVars = {urls:urlDatabase};
-  res.render("urls_index", templateVars);
-})
+function generateRandomString() {
+  let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890123456789";
+  let str = "";
+  for (i = 0; i < 6; i++) {
+    str += chars[Math.floor(Math.random() * 72)];
+  }
+}
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = {urls:urlDatabase};
+  res.render("urls_index", templateVars);
+})
 
 app.get("/", (req, res) => {
   res.send("Hello!");
