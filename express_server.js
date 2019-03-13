1fe3fc9c9986505981631c7c4352c7bd544757a2
Bosco <bleung9@gmail.com>
2019-03-13 23:59:58 +0000

    finished day 3 of tinyapp w/ preliminary cookies

--- express_server.js
+++ express_server.js
@@ -1,6 +1,13 @@
 var express = require("express");
+var cookieParser = require('cookie-parser');
 var app = express();
 var PORT = 8080; // default port 8080
+
+//require the body library to parse the body into a string (req.body)
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
@@ -18,20 +25,26 @@ function generateRandomString() {
   return str;
 }
 
-//require the body library to parse the body into a string (req.body)
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}))
-
 //when URL is /urls/new, render the stuff on urls_new.ejs.  urls_new is a form which
 //allows entry of new URL's.  upon submitting, it'll route to app.post("/urls")
 //this must come before app.get("/urls/:shortURL") b/c urls/new is a subset of that!!!!!!
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { };
+  if (!templateVars.username) {
+    templateVars.username = req.cookies["username"];
+  }
+  res.render("urls_new", templateVars);
 });
 
 ///when URL is urls/:shortURL, render the urls_show.ejs file
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]
+  };
+  if (!templateVars.username) {
+    templateVars.username = req.cookies["username"];
+  }
   res.render("urls_show", templateVars);
 });
 
@@ -40,6 +53,19 @@ app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
+
+//take a username from the login form, and set it to res.cookie
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+});
+
+//take a username from the logout form, and set it to res.cookie
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+});
+
 
 //upon receiving a new submission on urls_new,
 //store that new URL in a database w/ an alphanumeric ID
@@ -68,7 +94,10 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 
 //when URL is /urls, load the urls_index page w/ list of URL's in the database
 app.get("/urls", (req, res) => {
-  let templateVars = {urls:urlDatabase};
+  let templateVars = {
+    username: req.cookies["username"],
+    urls:urlDatabase
+  };
   res.render("urls_index", templateVars);
 })
 
