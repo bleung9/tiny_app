7cadd41838daf3c0311ecae6b99b1f0051fd8839
Bosco <bleung9@gmail.com>
2019-03-13 00:27:22 +0000

    redirect to short URL

--- express_server.js
+++ express_server.js
@@ -32,9 +32,9 @@ app.get("/urls/:shortURL", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
   let short = generateRandomString();
   urlDatabase[short] = req.body.longURL;
+  res.redirect(`/urls/${short}`);
 });
 
 app.get("/urls", (req, res) => {
